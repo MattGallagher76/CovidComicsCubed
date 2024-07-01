1d4e461e41c8a8c818232287ba5bb57056c6f930
MattGallagher76 <matthewg03@vt.edu>
2024-07-01 14:15:56 -0400

    New Data Visualizing stuff

--- Assets/StatisticVisualizer.cs
+++ Assets/StatisticVisualizer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,9 +17,20 @@ public class StatisticVisualizer : MonoBehaviour
     public float minimumRandom;
     public float maximumRandom;
 
+    public TextAsset csvFile;
+
     // Start is called before the first frame update
     void Start()
     {
+        if (csvFile != null)
+        {
+            //ProcessCSV(csvFile.text);
+        }
+        else
+        {
+            Debug.LogError("CSV file not assigned!");
+        }
+
         visualizedObjects = new List<GameObject>();
         for(int x = 0; x < width; x ++)
         {
@@ -37,7 +49,7 @@ public class StatisticVisualizer : MonoBehaviour
             testDataSet.Clear();
             for(int i = 0; i < width * depth; i ++)
             {
-                float k = Random.Range(minimumRandom, maximumRandom);
+                float k = UnityEngine.Random.Range(minimumRandom, maximumRandom);
                 testDataSet.Add(k);
                 if (k > max)
                     max = k;
@@ -46,6 +58,55 @@ public class StatisticVisualizer : MonoBehaviour
             for(int i = 0; i < width * depth; i ++)
             {
                 visualizedObjects[i].transform.localScale = new Vector3(1f, testDataSet[i] / max, 1f);
+            }
+        }
+    }
+
+    public Vector2 FindClosestFactors(int number)
+    {
+        int factor1 = 1;
+        int factor2 = number;
+        int minDifference = number - 1; // Initializing with the maximum possible difference
+
+        // Iterate over possible factors
+        for (int i = 1; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                int j = number / i;
+                int difference = Math.Abs(i - j);
+
+                // Check if the current pair has the smallest difference
+                if (difference < minDifference)
+                {
+                    factor1 = i;
+                    factor2 = j;
+                    minDifference = difference;
+                }
+            }
+        }
+
+        return new Vector2(factor1, factor2);
+    }
+
+    private void ProcessCSV(string csvData)
+    {
+        string[] lines = csvData.Split('\n');
+
+        Vector2 temp = FindClosestFactors(lines.Length);
+        width = (int)temp.x;
+        depth = (int)temp.y;
+
+        foreach (string line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line)) continue; // Skip empty lines
+
+            string[] fields = line.Split(',');
+            // Process each field here
+            // Example: Print fields to the console
+            foreach (string field in fields)
+            {
+                testDataSet.Add(float.Parse(line));
             }
         }
     }
