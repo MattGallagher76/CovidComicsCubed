0fae8240377aba2a476daf755712dc9bac70fadd
MattGallagher76 <mtg5212@gmail.com>
2024-07-22 15:54:28 -0400

    New graph engine which needs to be replaced

--- Assets/DataSetSelector.cs
+++ Assets/DataSetSelector.cs
@@ -1,4 +1,5 @@
 using Oculus.Interaction;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,7 +9,9 @@ public class DataSetSelector : MonoBehaviour
 {
     public string countryName;
     public List<graphValue> testDataSet;
-    public GameObject graphPoint;
+    public GameObject graphLine;
+    LineRenderer lr;
+    GameObject localGraphLine;
     public string handTag;
 
     public Vector3 offset;
@@ -18,7 +21,7 @@ public class DataSetSelector : MonoBehaviour
 
     public dataManager dm;
 
-    List<GameObject> graphPoints = new List<GameObject>();
+    List<Vector3> graphPoints = new List<Vector3>();
 
     //For graph, plot all points on a spline with no shown points or lines,
     //Then have an invisable object move accross the spline with a line being generated behind it
@@ -49,25 +52,41 @@ public class DataSetSelector : MonoBehaviour
     public void graphData()
     {
         dm.graph(this);
+        localGraphLine = Instantiate(graphLine);
+        lr = localGraphLine.GetComponent<LineRenderer>();
         foreach(graphValue gv in testDataSet)
         {
+            /*
             GameObject t = Instantiate(graphPoint);
             graphPoints.Add(t);
             t.transform.parent = this.transform.parent;
             t.transform.localPosition = new Vector3(gv.getTimeSinceStart() * xScale, gv.getCases() * yScale, 0) + offset;
+            */
+
+            Vector3 t = this.transform.parent.localPosition + new Vector3(gv.getTimeSinceStart() * xScale, gv.getCases() * yScale, 0) + offset;
+            graphPoints.Add(t);
+        }
+
+        //buba
+
+        lr.positionCount = graphPoints.Count;
+        int i = 0;
+        foreach(Vector3 v in graphPoints)
+        {
+            Debug.Log("I: " + i + ", V: " + v);
+            lr.SetPosition(i, v);
+            i++;
         }
     }
 
     public void clearGraph()
     {
-        foreach(GameObject gb in graphPoints)
-        {
-            Destroy(gb);
-        }
+        Destroy(localGraphLine);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other.gameObject.name);
         if (other.gameObject.CompareTag(handTag))
         {
             graphData();
